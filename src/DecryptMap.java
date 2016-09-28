904d355fede31adb0a41be927dd9463485bf681f
shuming2 <Amber@ZhangShumingdeMacBook-Air.local>
2016-09-28 10:18:17 -0600

    add "findWord"function & optimizing

--- src/DecryptMap.java
+++ src/DecryptMap.java
@@ -19,17 +19,61 @@ public class DecryptMap {
         }
     }
 
-    // Return all the possible combination of [p][k] under the same cipher byte
-    public ArrayList<Integer> getCombi(int cipher) {
-        /*
-        System.out.print("{");
-        for(int i: decryptmap.get(cipherbyte)) {
-            System.out.print(Integer.toHexString(i)+",");
+    public char getPlain(int cipher, int key) {
+        int ph = 0,pl = 0;
+        ArrayList<Integer> chcombi = decryptmap.get(cipher>>4);
+        ArrayList<Integer> clcombi = decryptmap.get(cipher&15);
+        for (int chc: chcombi) {
+            if ((chc&15)==(key&15)) {
+                ph = chc>>4;
+            }
         }
-        System.out.print("}");
-        */
-        return decryptmap.get(cipher);
+        for (int clc: clcombi) {
+            if ((clc&15)==(key>>4)) {
+                pl = clc>>4;
+            }
+        }
+        return (char) ((ph<<4)|pl);
     }
+
+    // Return all the possible combination of the key and plain on the specific index
+    public ArrayList<Integer>[] getAllCombi(int cipher) {
+        ArrayList<Integer> plainlist = new ArrayList<>();
+        ArrayList<Integer> keylist = new ArrayList<>();
+        int p,k;
+        int ch = cipher >> 4;
+        int cl = cipher & 15;
+
+        for (int chc: getCombi(ch)) {
+            for (int clc: getCombi(cl)) {
+                p = chc&0xf0 | (clc>>4);
+                k = (clc&0x0f)<<4 | (chc&0x0f);
+                if (isPrintable(p)&& (isPrintable(k)||isNonPrintable(k))) {
+                    plainlist.add(p);
+                    keylist.add(k);
+                }
+            }
+        }
+        ArrayList<Integer>[] result = new ArrayList[2];
+        result[0] = keylist;
+        result[1] = plainlist;
+        return result;
+    }
+
+    private boolean isPrintable(int text) {
+        return (text > 31) && (text < 128);
+    }
+
+    private boolean isNonPrintable(int text) {
+        return text < 32;
+    }
+
+    // Return all the possible combination of [p][k] under the same cipher byte
+    private ArrayList<Integer> getCombi(int cipherbyte) {
+        return decryptmap.get(cipherbyte);
+    }
+
+
 }
 
 
